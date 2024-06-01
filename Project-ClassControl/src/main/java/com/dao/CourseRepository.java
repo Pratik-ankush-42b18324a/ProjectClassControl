a88ec0e7c0de8974e2ce6a17f2281c3efbeed98f
Pratik-ankush-42b18324a <116563055+Pratik-ankush-42b18324a@users.noreply.github.com>
2024-06-01 14:06:05 +0530

    Update CourseRepository.java

--- Project-ClassControl/src/main/java/com/dao/CourseRepository.java
+++ Project-ClassControl/src/main/java/com/dao/CourseRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.model.Course;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Integer>{
 
 	
