15f08f346a818be3859c06745b384031f1bac1ca
Pratik-ankush-42b18324a <116563055+Pratik-ankush-42b18324a@users.noreply.github.com>
2024-06-01 13:43:33 +0530

    Update PasswordGenerator.java

--- Project-ClassControl/src/main/java/com/service/PasswordGenerator.java
+++ Project-ClassControl/src/main/java/com/service/PasswordGenerator.java
@@ -4,7 +4,7 @@ import java.security.SecureRandom;
 
 public class PasswordGenerator {
 	
-	private static final String LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+    private static final String LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
     private static final String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String DIGITS = "0123456789";
     private static final String SPECIAL_SYMBOLS = "!@#$%^&*()-_+=";
