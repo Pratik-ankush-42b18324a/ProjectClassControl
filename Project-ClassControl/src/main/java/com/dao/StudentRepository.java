b28e2b6111ac1573e5b4f5227db9193fb5a02dcb
Pratik-ankush-42b18324a <116563055+Pratik-ankush-42b18324a@users.noreply.github.com>
2024-06-01 14:07:07 +0530

    Update StudentRepository.java

--- Project-ClassControl/src/main/java/com/dao/StudentRepository.java
+++ Project-ClassControl/src/main/java/com/dao/StudentRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.model.Student;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-	@Query("SELECT s FROM Student s WHERE s.student_Email = :student_Email")
+	
+    @Query("SELECT s FROM Student s WHERE s.student_Email = :student_Email")
     Student findByStudent_Email(String student_Email);
+	
 }
