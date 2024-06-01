b5c15e3639a211352eb9ccb5f099ab1d0e9ce581
Pratik-ankush-42b18324a <116563055+Pratik-ankush-42b18324a@users.noreply.github.com>
2024-06-01 14:05:22 +0530

    Update AssignmentRepository.java

--- Project-ClassControl/src/main/java/com/dao/AssignmentRepository.java
+++ Project-ClassControl/src/main/java/com/dao/AssignmentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.model.Assignment;
 
+@Repository
 public interface AssignmentRepository extends JpaRepository<Assignment, Integer> {
 
 }
